1c34fde33331b3fee9c8caac4f49ccf77aaf17c7
NISHIKAWA Hitoshi <hitoshi@Hitoshis-MacBook-Pro-2018.local>
2019-06-04 13:00:07 +0900

    Add bleu-1 and enable to process raw inputs.

--- lib/sumire.py
+++ lib/sumire.py
@@ -19,7 +19,7 @@ class Sumire:
 		self.parse_mecab_pattern = re.compile(r'\t|,')
 
 		parser = OptionParser()
-		parser.set_defaults(input_type='mecab',
+		parser.set_defaults(input_type='default',
 							rouge_n=1)
 		parser.add_option('-c', '--cabocha',
 						  action='store_const',
@@ -48,37 +48,14 @@ class Sumire:
 		(self.options, self.args) = parser.parse_args()
 
 	def calculate_rouge(self, **kwargs):
-
-		# Interpret options from kwargs
-		self.options.input_type = kwargs['input_type'] if 'input_type' in kwargs else self.options.input_type
-
-		# Reference and summary are given as files
-		if self.options.reference_file and self.options.summary_file:
-			lines = self._read_input(self.options.reference_file)
-			r_dist = self._make_distribution(lines)
-
-			lines = self._read_input(self.options.summary_file)
-			s_dist = self._make_distribution(lines)
-
-		# Reference and summary are given as sentences
-		elif ('reference' in kwargs and 'summary' in kwargs):
-			lists = self._parse_sentence(reference)
-			lines = self._read_input_from_lines(lists)
-			r_dist = self._make_distribution(lines)
-
-			lists = self._parse_sentence(summary)
-			lines = self._read_input_from_lines(lists)
-			s_dist = self._make_distribution(lines)
-
-		# Reference and summary are given as lists
-		elif ('reference_list' in kwargs and 'summary_list' in kwargs):
-			lines = self._read_input_from_lines(kwargs['reference_list'])
-			r_dist = self._make_distribution(lines)
-
-			lines = self._read_input_from_lines(kwargs['summary_list'])
-			s_dist = self._make_distribution(lines)
+		r_dist, s_dist = self._select_input_type(reference = kwargs['reference'], summary = kwargs['summary'])
 		
 		return self._calculate_overlap(r_dist, s_dist)
+
+	def calculate_bleu(self, **kwargs):
+		r_dist, s_dist = self._select_input_type(reference = kwargs['reference'], summary = kwargs['summary'])
+
+		return self._calculate_overlap(s_dist, r_dist)
 
 	def _calculate_overlap(self, r_dist, s_dist):
 		overlap = 0
@@ -151,7 +128,6 @@ class Sumire:
 		lines = []
 		f = open(file_path, 'r')
 		for line in f.readlines():
-			#line = str(line, 'utf-8')
 			line = line.rstrip()
 			a_match = self.a_pattern.match(line)
 			if a_match == None:
@@ -167,18 +143,45 @@ class Sumire:
 				lines.append(line)
 		return lines
 
-	#def _read_input_from_lines(self, array):
-	#	lines = []
-	#	for i in range(0, len(array)):
-	#		for j in range(0, len(array[i])):
-	#			a_match = self.a_pattern.match(array[i][j])
-	#			if a_match == None:
-	#				lines.append(array[i][j])
-	#	return lines
+	def _select_input_type(self, **kwargs):
+
+		# Interpret options from kwargs
+		self.options.input_type = kwargs['input_type'] if 'input_type' in kwargs else self.options.input_type
+
+		# Reference and summary are given as files
+		if self.options.reference_file and self.options.summary_file:
+			lines = self._read_input(self.options.reference_file)
+			r_dist = self._make_distribution(lines)
+
+			lines = self._read_input(self.options.summary_file)
+			s_dist = self._make_distribution(lines)
+
+		# Reference and summary are given as sentences (space as word delimiter)
+		elif ('reference' in kwargs and 'summary' in kwargs):
+			#lists = self._parse_sentence(reference)
+			lines = self._read_input_from_lines([reference])
+			r_dist = self._make_distribution(lines)
+
+			#lists = self._parse_sentence(summary)
+			lines = self._read_input_from_lines([summary])
+			s_dist = self._make_distribution(lines)
+
+		# Reference and summary are given as lists
+		elif ('reference_list' in kwargs and 'summary_list' in kwargs):
+			lines = self._read_input_from_lines(kwargs['reference_list'])
+			r_dist = self._make_distribution(lines)
+
+			lines = self._read_input_from_lines(kwargs['summary_list'])
+			s_dist = self._make_distribution(lines)
+
+		return r_dist, s_dist
 
 if __name__ == '__main__':
 	smr = Sumire()
-	reference = "今日はとても暑いが私は好きです．"
-	summary = "今日はとても寒くて私は嫌いです．"
+	# This is an example. Reference and summary sentences are input as delimited with space.
+	reference = "今日 は とても 暑い が 私 は 好き です ．"
+	summary = "今日 は とても 寒くて 私 は 嫌い です ．"
 	print((smr.calculate_rouge(reference = reference, 
 							   summary = summary)))
+	print((smr.calculate_bleu(reference = reference,
+							  summary = summary)))
